aa8ca7492eb73afa1abe1c0995d92edd6d34944e
xSylla <cryptosylla@gmail.com>
2025-03-04 15:41:26 +0100

    removed gitingest

--- documentation/mvxsdktester/types/errors.ts
+++
@@ -1,9 +0,0 @@
-export class InvalidPatternError extends Error {
-  constructor(pattern: string) {
-    super(
-      `Pattern '${pattern}' contains invalid characters. Only alphanumeric characters, ` +
-      'dash (-), underscore (_), dot (.), forward slash (/), plus (+), and asterisk (*) are allowed.'
-    );
-    this.name = 'InvalidPatternError';
-  }
-} 
